17c9676f3e1b6a652ffd72bf21529638d90a4b87
lixun <lixun@lixundeimac.local>
2017-03-19 11:44:00 +0800

    Initial Commit

---
+++ testTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testTimer
+//
+//  Created by lixun on 2017/3/19.
+//  Copyright © 2017年 lixun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
